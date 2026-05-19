ff081116e1747aa980fe3405176dc5765c65ba37
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2026-05-19 11:04:50 +0200

    feat(home): enable SSH commit signing

--- home/git.nix
+++ home/git.nix
@@ -2,10 +2,16 @@ _: {
   programs.git = {
     enable = true;
     settings = {
-      user.name = "Karl Gunnarsson";
-      user.email = "karlgunnarsson98@gmail.com";
+      user = {
+        name = "Karl Gunnarsson";
+        email = "karlgunnarsson98@gmail.com";
+        signingKey = "~/.ssh/id_ed25519.pub";
+      };
       init.defaultBranch = "main";
       core.editor = "nvim";
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
     };
   };
 }
