9ed2ccb6dca5082b9e451f483d4fe73128e80cd8
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2026-05-22 10:52:10 +0200

    feat(darwin): add jankyborders with catppuccin blue gradient borders
    - active border: blue to sapphire gradient (full opacity)
    - inactive border: same gradient at 60% opacity to cover native macOS border

--- darwin/default.nix
+++ darwin/default.nix
@@ -7,5 +7,6 @@ _: {
     ./macos.nix
     ./yabai.nix
     ./skhd.nix
+    ./jankyborders.nix
   ];
 }
---
+++ darwin/jankyborders.nix
@@ -0,0 +1,10 @@
+_: {
+  services.jankyborders = {
+    enable = true;
+    hidpi = true;
+    order = "above";
+    width = 5.0;
+    active_color = "gradient(top_left=0xff89B4FA,bottom_right=0xff74C7EC)";
+    inactive_color = "gradient(top_left=0x9989B4FA,bottom_right=0x9974C7EC)";
+  };
+}
